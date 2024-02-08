732485b650a9c24c79842f1a0aa11f768c54e8bb
Ethan Lukas <elukas@uci.edu>
2024-02-07 23:59:37 -0800

    Initial Commit

---
+++ FitQuest/FitQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitQuestApp.swift
+//  FitQuest
+//
+//  Created by Ethan Lukas on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FitQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
